2f3f0a2fda49c904c432868bccab1ab3e04be7dd
Daniel Bogucki <daniel.bogucki14@gmail.com>
2022-02-02 15:03:55 +0100

    Added initial files // HTML template + CSS styles

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const holes = document.querySelectorAll('.hole');
+const moles = document.querySelectorAll('.mole');
+const score = document.querySelector('.score');
+
+let lastHole;
+let timesUp = false;
+let score = 0;
